434191f809829ae781e53566fe3454456cb502da
danji404 <mire090909@gmail.com>
2025-02-16 05:43:46 +0900

    텍스트를 멋지게 수정

--- Assets/Shader/Materials/BlendModes/BlendModeController.cs
+++ Assets/Shader/Materials/BlendModes/BlendModeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-// Note: the only ones that work on TMPro are Lighten, Additive, Soft Additive, Linear Burn
 public enum BlendingMode
 {
     Normal,
@@ -14,75 +13,81 @@ public enum BlendingMode
     Soft_Additive,
     Linear_Burn,
     Overlay,
-
 }
+
 public class BlendModeController : MonoBehaviour
 {
     public BlendingMode blendMode;
     public Material material;
 
+    void Start()
+    {
+        ApplyBlendMode();
+    }
 
     void OnValidate()
     {
-        BlendMode srcMode = BlendMode.SrcAlpha,
-        dstMode = BlendMode.OneMinusSrcAlpha;
-        BlendOp op = 0;
+        ApplyBlendMode();
+    }
+
+    void ApplyBlendMode()
+    {
         if (material == null)
         {
-            material = GetComponent<Renderer>().sharedMaterial;
+            var renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                material = renderer.material; // 실행 중에도 반영되도록 변경
+            }
+            if (material == null) return;
         }
+
+        BlendMode srcMode = BlendMode.SrcAlpha, dstMode = BlendMode.OneMinusSrcAlpha;
+        BlendOp op = BlendOp.Add;
 
         switch (blendMode)
         {
             case BlendingMode.Darken:
-                {
-                    srcMode = BlendMode.One;
-                    dstMode = BlendMode.One;
-                    op = BlendOp.Min;
-                }
+                srcMode = BlendMode.One;
+                dstMode = BlendMode.One;
+                op = BlendOp.Min;
                 break;
             case BlendingMode.Multiply:
-                {
-                    srcMode = BlendMode.DstColor;
-                    dstMode = BlendMode.Zero;
-                }
+                srcMode = BlendMode.DstColor;
+                dstMode = BlendMode.Zero;
                 break;
             case BlendingMode.Lighten:
-                {
-                    srcMode = BlendMode.One;
-                    dstMode = BlendMode.One;
-                    op = BlendOp.Max;
-                }
+                srcMode = BlendMode.One;
+                dstMode = BlendMode.One;
+                op = BlendOp.Max;
                 break;
             case BlendingMode.Additive:
-                {
-                    srcMode = BlendMode.One;
-                    dstMode = BlendMode.One;
-                }
+                srcMode = BlendMode.One;
+                dstMode = BlendMode.One;
                 break;
             case BlendingMode.Soft_Additive:
-                {
-                    srcMode = BlendMode.OneMinusDstColor;
-                    dstMode = BlendMode.One;
-                }
+                srcMode = BlendMode.OneMinusDstColor;
+                dstMode = BlendMode.One;
                 break;
             case BlendingMode.Linear_Burn:
-                {
-                    srcMode = BlendMode.One;
-                    dstMode = BlendMode.One;
-                    op = BlendOp.ReverseSubtract;
-                }
+                srcMode = BlendMode.One;
+                dstMode = BlendMode.One;
+                op = BlendOp.ReverseSubtract;
                 break;
             case BlendingMode.Overlay:
-                {
-                    srcMode = BlendMode.One;
-                    dstMode = BlendMode.SrcColor;
-                }
+                srcMode = BlendMode.One;
+                dstMode = BlendMode.SrcColor;
                 break;
-            default: break;
+            default:
+                break;
         }
+
         material.SetInt("_SrcMode", (int)srcMode);
         material.SetInt("_DstMode", (int)dstMode);
         material.SetInt("_BlendOp", (int)op);
+
+        // 디버깅용 로그 출력
+        Debug.Log($"Blending Mode Applied: {blendMode}");
+        Debug.Log($"_SrcMode: {srcMode}, _DstMode: {dstMode}, _BlendOp: {op}");
     }
 }
